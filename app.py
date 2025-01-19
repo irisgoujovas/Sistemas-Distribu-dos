95bca42fd98a73f180bce9ca2086ad4105fcbb02
GersonPaulo <92855428+GersonPaulo@users.noreply.github.com>
2025-01-19 19:05:57 +0000

    comments user app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from controller import register_user, login_user, change_password
 
+#
 if __name__ == "__main__":
 #def menu():
+    # menu de opções
     while True:
         print("""\nEscolha uma opção:
         1. Registrar Usuário
@@ -10,23 +12,23 @@ if __name__ == "__main__":
         4. Sair""")
         
         choice = input("Opção: ")
-        
+#regista o usuario        
         if choice == "1":
             user_name = input("Nome do usuário: ")
             user_email = input("Email do usuário: ")
             user_password = input("Senha do usuário: ")
             register_user(user_name, user_email, user_password)
-
+#Faz o login do usuario, caso exista
         elif choice == "2":
             user_email = input("Email: ")
             user_password = input("Senha: ")
             login_user(user_email, user_password)
-
+#altera a senha de um usuario já existente
         elif choice == "3":
             user_email = input("Email: ")
             new_password = input("Nova senha: ")
             change_password(user_email, new_password)
-
+#Sai do menu e faz um break
         elif choice == "4":
             print("Saindo...")
             break
